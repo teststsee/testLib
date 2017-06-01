109566e1e9cc84a3c5b5e9ce6b2e3e2d9baca320
teststseee <trabantxxl@azet.sk>
2017-06-01 14:27:04 +0200

    added lib

---
+++ test/src/main/java/com/example/test/TestMessage.java
@@ -0,0 +1,15 @@
+package com.example.test;
+
+import android.util.Log;
+
+/**
+ * Created by hotovo on 01/06/2017.
+ */
+
+public class TestMessage {
+    private static final String Nessage="YOLO";
+
+    public static void printMessage(){
+        System.out.println(Nessage);
+    }
+}
